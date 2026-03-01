48e77cbeff91dc3ebe62756abf2d5f7cb3b7629e
VictorAgahi <victor@agahi.com>
2026-03-01 14:04:54 +0100

    feat(proto): new libs

---
+++ packages/contracts/src/index.ts
@@ -0,0 +1,4 @@
+export * from './event/index.js';
+export * from './post/index.js';
+export * from './user/index.js';
+export * from './common/index.js';
---
+++ packages/contracts/src/user/index.ts
@@ -0,0 +1,4 @@
+export * from './user.types.js';
+export * from './user.requests.js';
+export * from './user.responses.js';
+export * from './user.services.js';
