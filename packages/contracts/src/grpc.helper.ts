c5c488ccaa9b08301035c615a784e32d79f79821
VictorAgahi <victor@agahi.com>
2026-03-01 16:31:54 +0100

    grpc contracts

---
+++ packages/contracts/src/grpc.helper.ts
@@ -0,0 +1,43 @@
+import { join } from 'path';
+import type { GrpcOptions } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
+
+export const GRPC_SERVICES = {
+  USER: {
+    package: 'volontariapp.user',
+    protoFileName: 'user.services.proto',
+    domain: 'user',
+  },
+  POST: {
+    package: 'volontariapp.post',
+    protoFileName: 'post.services.proto',
+    domain: 'post',
+  },
+  EVENT: {
+    package: 'volontariapp.event',
+    protoFileName: 'event.services.proto',
+    domain: 'event',
+  },
+};
+
+export const getGrpcOptions = (service: keyof typeof GRPC_SERVICES, url: string): GrpcOptions => {
+  const config = GRPC_SERVICES[service];
+  const protoRoot = join(__dirname, '../proto');
+
+  return {
+    transport: Transport.GRPC,
+    options: {
+      url,
+      package: config.package,
+      protoPath: join(protoRoot, `volontariapp/${config.domain}/${config.protoFileName}`),
+      loader: {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+        includeDirs: [protoRoot],
+      },
+    },
+  };
+};
